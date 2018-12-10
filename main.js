487b1639497d4fb8318a5b28c5652fe90299d3c3
Warren Tai <warren.w.tai@gmail.com>
2018-12-10 17:41:25 -0500

    almost finished, probably can layer carousels on top on cards

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var wings = axios.get("https://www.redbullshopus.com/products.json");
 
-
 wings.then((res) => {
-    if (res.status === 200) {
-        console.log("connection successful.");
-        console.dir(res.data.products[0].images[0].src);
-        var imgSrc = res.data.products[0].images[0].src;
-        var product = document.createElement('img');
-        product.src = imgSrc;
-        var frame = document.querySelector('#productframe');
-        console.log("frame:", frame);
-        frame.appendChild(product);
-    } else {
-        console.log("connection problems.")
+    var productData = res.data.products;
+    console.log(productData);
+    console.log(productData[1].images[0].src);
+    var productFrames = document.querySelectorAll('.col');
+    for (var i = 1; i < productFrames.length - 1; i++) {
+        productFrames[i].innerHTML = ``;
+        productFrames[i].innerHTML += `<div class="card">`;
+        productFrames[i].innerHTML += `<img class="card-img-top" src="${productData[i-1].images[0].src}" alt="Card image cap">`;
+        productFrames[i].innerHTML += `<div class="card-body">`;
+        productFrames[i].innerHTML += `<h5 class="card-title">${productData[i-1].title}</h5>`;
+        productFrames[i].innerHTML += `<a href="#" class="btn btn-primary">$${productData[i-1].variants[0].price}</a>`;
+        productFrames[i].innerHTML += `<p class="card-text">${productData[i-1].body_html}</p>`;
+        productFrames[i].innerHTML += `</div>`;
+        productFrames[i].innerHTML += `</div>`;
     }
 });
+    
+
